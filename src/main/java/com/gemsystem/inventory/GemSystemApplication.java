494cfa13b6bb93b3581fdd34e91257045e30d5cb
Chathurya <it24101093@my.sliit.lk>
2026-02-16 14:22:12 +0530

    Initial Project Skeleton

---
+++ src/main/java/com/gemsystem/inventory/GemSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gemsystem.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GemSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gemsystem/inventory/GemSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemsystem.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
